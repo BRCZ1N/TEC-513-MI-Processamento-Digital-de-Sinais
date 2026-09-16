8425a9e8085df019f79d3d243d1ea7c39a3d4d96
Bruno <brunocamposo.r@gmail.com>
2026-09-16 03:06:58 -0300

    Ideal quase finalizada falta alterar algumas coisas ajustar umas proporções

--- Main.m
+++ Main.m
@@ -4,56 +4,128 @@ close all;
 
 pkg load signal;
 
-f_analog = 1e5;            % Frequência analógica simulada (100 kHz)
-Ta = 1/f_analog;           % Período de amostragem analógico
-T_total = 0.01;             % Tempo total de simulação (1s)0.1
-t = 0:Ta:T_total-Ta;        % Vetor de tempo "contínuo"
-N_analog = length(t);       % Número total de pontos analógicos
 
 % Frequências do sinal analógico de entrada
 f1 = 1e3;                   % Senoide desejada (Banda do sinal: 1KHz)
-f2 = 10e3;                  % Segunda senoide (Banda do sinal: 10KHz)
+f2 = 5e3;                   % Segunda senoide (Banda do sinal: 5KHz)
 % Amplitudes das senoides
 A1 = 1.0;
 A2 = 1.2;
 
-senoideA = A1*sin(2*pi*f1*t);
+fAnalog = 1e5;             % Frequência analógica simulada (100 kHz)
+Ta = 1/fAnalog;            % Período de amostragem analógico
+tempoTotal = 1;            % Tempo total de simulação (1s)
+t = 0:Ta:tempoTotal-Ta;    % Vetor de tempo "contínuo"
+nPAnalog = length(t);      % Número total de pontos analógicos
+fs = 10e3;                 % Frequencia de amostragem(Trem de impulsos)
+Ts = 1/fs;                 % Periodo de amostragem(Trem de impulsos)
+nPlots = 8;                % Número total de plots
+ordemFiltro = 100;
+% fs >= 2*f1
+fNyquist = fs/2;
+fCorte = 1e3;
+fs = 10e3;
+Ts = 1/fs;
+kPasso = round(Ts/Ta);
+
+sinalA = A1*sin(2*pi*f1*t);
 
 %figure;
-subplot(3,1,1);
-plot(t, senoideA);
+subplot(nPlots,1,1);
+plot(t, sinalA);
 grid on;
 xlabel('Tempo (s)');
 ylabel('Amplitude');
-title('Sinal analógico A');
+title('Sinal A');
+xlim([0 0.01]);
 
-senoideB = A2*sin(2*pi*f2*t);
+sinalB = A2*sin(2*pi*f2*t);
 
 %figure;
-subplot(3,1,2);
-plot(t, senoideB);
+subplot(nPlots,1,2);
+plot(t, sinalB);
 grid on;
 xlabel('Tempo (s)');
 ylabel('Amplitude');
-title('Sinal analógico B');
+title('Sinal B');
+xlim([0 0.01]);
 
-senoideComposta = senoideA + senoideB;
+sinalComposto = sinalA + sinalB;
 
 %figure;
-subplot(3,1,3);
-plot(t, senoideComposta);
+subplot(nPlots,1,3);
+plot(t,sinalComposto);
 grid on;
 xlabel('Tempo (s)');
 ylabel('Amplitude');
-title('Sinal analógico composto');
+title('Sinal composto');
+xlim([0 0.01]);
 
-fs = 150;                   % Frequência de amostragem ajustável (Hz)
-Ts = 1/fs;                  % Período de amostragem digital (s)
-k_passo = round(Ts/Ta);
+filtro = fir1(ordemFiltro, fCorte/fNyquist);
+sinalFiltrado = filter(filtro, 1, sinalComposto);
 
-trem_impulsos = zeros(1, N_analog);
-k_passo = round(Ts/Ta);
-trem_impulsos(1:k_passo:end) = 1;
+tremImpulsos = zeros(1, nPAnalog);
+tremImpulsos(1:kPasso:end) = 1;
+
+subplot(nPlots,1,4);
+stem(t, tremImpulsos);
+grid on;
+xlabel('Tempo (s)');
+ylabel('Amplitude');
+title('Trem de impulsos');
+xlim([0 0.01]);
+
+sinalAmostrado = sinalFiltrado .* tremImpulsos;
+
+subplot(nPlots,1,5);
+stem(t, sinalAmostrado);
+grid on;
+xlabel('Tempo (s)');
+ylabel('Amplitude');
+title('Sinal amostrado');
+xlim([0 0.01]);
+
+f = (-nPAnalog/2 : nPAnalog/2 - 1) * (fAnalog / nPAnalog)
+
+sinalCompostoFTT = abs(fftshift(fft(sinalComposto)))/nPAnalog;
+
+subplot(nPlots,1,6);
+stem(f, sinalCompostoFTT);
+grid on;
+xlabel('Frequência (Hz)');
+ylabel('Magnitude');
+title('Espectro do Sinal Composto');
+xlim([-10000 10000]);
+xticks(-10000 : 1000 : 10000);
+
+tremImpulsosFTT = abs(fftshift(fft(tremImpulsos)))/nPAnalog;
+
+subplot(nPlots,1,7);
+stem(f, tremImpulsosFTT);
+grid on;
+xlabel('Frequência (Hz)');
+ylabel('Magnitude');
+title('Espectro do Trem de Impulsos');
+xlim([-20000 20000]);
+xticks(-200000 : 10000 : 200000);
+
+espectroSinalAmostrado = conv(sinalCompostoFTT,tremImpulsosFTT);
+
+nP_conv = length(espectroSinalAmostrado);
+f_conv = linspace(-fAnalog, fAnalog, nP_conv);
+
+subplot(nPlots,1,8);
+stem(f_conv, espectroSinalAmostrado);
+grid on;
+xlabel('Frequência (Hz)');
+ylabel('Magnitude');
+title('Convolução dos sinais');
+xlim([-40000 40000]);
+xticks(-40000 : 10000 : 40000);
+
+
+
+
 
 
 
